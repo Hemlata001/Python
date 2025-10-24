27b7b7a5e35a102d203da3de893ad90d94f700c2
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-10-24 19:24:29 +0530

    Create startEnd.py

---
+++ startEnd.py
@@ -0,0 +1,6 @@
+#Ask start number and end number from user.print all the numbers from start and end using while loop.
+start = int(input("Enter the start number : "))
+end = int(input("Enter the end number : "))
+while start<=end:
+    print(start)
+    start+=1
