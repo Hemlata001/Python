f47c650e91740114a9c1a61695ca6c9ab238bb49
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2025-10-24 21:11:57 +0530

    Create divisible4.py

---
+++ divisible4.py
@@ -0,0 +1,9 @@
+# write a program to calculate the sum of all the numbers divisible by 4 from  20 to 50
+i = 20
+sum = 0
+while i <= 50:
+    if i % 4 == 0:
+        sum += i
+    i += 1
+print("sum of numbers divisible by 4 from 20 to 50 is:", sum)
+    
